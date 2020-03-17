cac2cbfcf7d7ffa234f3014b89f86a8c33576474
xuren <1300360509@qq.com>
2020-03-17 11:19:41 +0800

    更新结构

---
+++ src/main/java/com/xuren/gameserver/net/NetMsgDispatcher.java
@@ -0,0 +1,10 @@
+package com.xuren.gameserver.net;
+
+import com.google.protobuf.Message;
+import com.xuren.gameserver.proto.ProtoMsg3;
+
+public class NetMsgDispatcher {
+    public static void dispatcher(ProtoMsg3.HeadType headType, Message message) {
+
+    }
+}
---
+++ src/main/java/com/xuren/gameserver/net/NetworkMsgMap.java
@@ -0,0 +1,10 @@
+package com.xuren.gameserver.net;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class NetworkMsgMap {
+    private static Map<String, Class> map = new HashMap<>();
+    static {
+    }
+}
