4bbb7a4fc375123aa710652cc0397995c9e6c413
xuren <1300360509@qq.com>
2020-03-17 11:19:14 +0800

    更新结构

--- src/main/java/com/xuren/gameserver/net/handler/ProtoResolve2Handler.java
+++ src/main/java/com/xuren/gameserver/net/handler/ProtoResolve2Handler.java
@@ -1,5 +1,6 @@
 package com.xuren.gameserver.net.handler;
 
+import com.xuren.gameserver.net.NetMsgDispatcher;
 import com.xuren.gameserver.proto.ProtoMsg3;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
@@ -8,19 +9,15 @@ public class ProtoResolve2Handler extends ChannelInboundHandlerAdapter {
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
         //判断消息实例
-        if (null == msg || !(msg instanceof ProtoMsg3.ProtoMsg.Message)) {
+        if (null == msg || !(msg instanceof ProtoMsg3.Message)) {
             super.channelRead(ctx, msg);
             return;
         }
 
         //判断消息类型
-        ProtoMsg3.ProtoMsg.Message pkg = (ProtoMsg3.ProtoMsg.Message) msg;
-        ProtoMsg3.ProtoMsg.HeadType headType = ((ProtoMsg3.ProtoMsg.Message) msg).getType();
-        if(headType.equals(ProtoMsg3.ProtoMsg.HeadType.LOGIN_REQUEST)) {
-            //登录
-
-        }
-        // 根据类型分发消息
-
+        ProtoMsg3.Message pkg = (ProtoMsg3.Message) msg;
+        ProtoMsg3.HeadType headType = ((ProtoMsg3.Message) msg).getType();
+        // 分发消息
+        NetMsgDispatcher.dispatcher(headType, pkg);
     }
 }
